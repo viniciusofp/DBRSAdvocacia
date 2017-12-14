3305e03e6e733e7077fbc9ffd2eb929e643c40f2
viniciusofp <viniciusofp@gmail.com>
2017-12-14 15:11:30 -0200

    grunt updates

---
+++ dev2/gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+  var css = grunt.file.read('style.css' , []);
+
+  grunt.initConfig({
+    replace: {
+      dist: {
+        options: {
+          patterns: [
+            {
+              match: 'mycss',
+              replacement: css
+            }
+          ]
+        },
+        files: [
+          {expand: true, flatten: true, src: ['index.html'], dest: 'build/'}
+        ]
+      }
+    },
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'compressed'
+        },
+        files: {                         // Dictionary of files
+          'style.css': 'style.scss'       // 'destination': 'source'
+        }
+      }
+    },
+    watch: {
+      files: ['style.scss'],
+      tasks: ['sass']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-replace');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.registerTask('default', ['sass', 'link_html']);
+};
